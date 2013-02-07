667fd91e8e02499c846f3ff91ce819430ebec5e5
Erno Hopearuoho <erno.hopearuoho@gmail.com>
2013-02-07 17:12:25 +0200

    Added clickable icon, inital hacky version

--- data/show-password.js
+++ data/show-password.js
@@ -14,18 +14,52 @@
         }
     };
 
-    showPwd.fixElement = function(element, time) {
-        element.addEventListener('click', function(e) {
-            showPwd.peekPassword(element, time);
-        });
+    function fixElement(element, time) {
+        addIcon(element, time);
     }
 
-	function showPassword(element){		
-		element.type = 'text';
-	}
-	
-	function hidePassword(element){
-		element.type = 'password';
-	}
-	
+    function insertAfter(referenceNode, newNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+
+    function addIcon(element, time) {
+        let icon = document.createElement("input");
+        icon.type = "button";
+        icon.class = "isa-pwicon";
+
+        icon.style.cssText = "background-size: auto 100%, auto;";
+        icon.style.border = "none";
+        icon.style.backgroundImage = "url(http://i.imgur.com/VAZE0IX.png)";
+        icon.style.backgroundPosition = "center center";
+        icon.style.backgroundRepeat = "no-repeat";
+        icon.style.backgroundColor = "transparent";
+        icon.style.cursor = "pointer";
+        icon.style.position = "absolute";
+
+        let height = element.offsetHeight;
+        icon.style.width = height + "px";
+        icon.style.height = height + "px";
+
+        let right = element.offsetLeft + element.offsetWidth;
+        let top = element.offsetTop;
+        icon.style.top = top + "px";
+        icon.style.left = right-height +"px";
+
+        icon.addEventListener('click', function(e) {
+            showPwd.peekPassword(element, time);
+            element.focus();
+        });
+
+        insertAfter(element, icon);
+    }
+
+    function showPassword(element){
+            element.type = 'text';
+    }
+    
+    function hidePassword(element){
+            element.type = 'password';
+    }
+
+
 }(window.showPwd = window.showPwd || {}, fieldFinder));
