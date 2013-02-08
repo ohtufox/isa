824db16c40d1bd4815bcae3fc079960a6dae278b
Juho Kallio <juhopkallio@gmail.com>
2013-02-08 14:47:07 +0200

    Test refactoring...

--- test/qunit/tests/field-finder.js
+++
@@ -1,5 +0,0 @@
-let passwordFields = fieldFinder.findPasswordFields();
-let passwordFieldCount = passwordFields.length;
-test( "password field count test", function() {
-   ok( passwordFieldCount === 1, "Passed!" );
-});
