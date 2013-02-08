2718438f06abc80996111b4329973e65672b97c0
ohtufox <tmalmivi@cs.helsinki.fi>
2013-02-08 16:21:16 +0200

    fixed peekPassword call

--- data/my-script.js
+++ data/my-script.js
@@ -3,6 +3,6 @@ let passwordFields = fieldFinder.findPasswordFields();
 for (let i = 0; i < passwordFields.length; i++) {
     let element = passwordFields[i];
     element.addEventListener('dblclick', function(e) {
-		showPwd.showPassword(element);
+		showPwd.peekPassword(element);
     });
 }
