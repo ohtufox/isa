e04ecfad6e52823bcc18956d27c89cf8eb97dea3
ohtufox <tmalmivi@cs.helsinki.fi>
2013-02-14 18:39:32 +0200

    show tooltip and send message to main.js on doubleclick

--- data/show-password.js
+++ data/show-password.js
@@ -9,12 +9,13 @@
         let passwordFields = finder.findPasswordFields();
         for(let i = 0; i < passwordFields.length; i++) {
             fixElement(passwordFields[i], time);
+			sendInfo(passwordFields[i]);
         }
     };
 
     function fixElement(element, time) {
         element.addEventListener('click', function(e) {
-            showPwd.peekPassword(element, time);
+			showPwd.peekPassword(element, time);
         });
     }
 
@@ -24,6 +25,14 @@
 	
 	function hidePassword(element){
 		element.type = 'password';
+	}
+	
+	function sendInfo(element) {
+		element.addEventListener('dblclick', function(e) {
+			let msg = "click to show password";
+			element.title = msg;
+			self.port.emit('info', msg);
+        });
 	}
 }(window.showPwd = window.showPwd || {}, fieldFinder));
 
