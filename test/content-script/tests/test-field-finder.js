790f7c05b83a1ed383dfdf33fe42d06417f502b9
Juho Kallio <juhopkallio@gmail.com>
2013-02-21 14:39:27 +0200

    Fixed the conflicted files...

--- test/content-script/tests/test-field-finder.js
+++ test/content-script/tests/test-field-finder.js
@@ -1,16 +1,6 @@
 let passwordFields = fieldFinder.findPasswordFields();
 let passwordFieldCount = passwordFields.length;
-<<<<<<< HEAD:test/content-script/tests/test-field-finder.js
 
-test( "Standard password field find test", function() {
-   ok( passwordFieldCount == 1, "Passed!" );
-});
-=======
 test( "Standard password field count test", function() {
    ok( passwordFieldCount >= 1, "Passed!" );
 });
-test( "JavaScript created password field count test", function() {
-   ok( passwordFieldCount === 3, "Passed!" );
-});
->>>>>>> JavaScript generated field finder test.:test/content-script/tests/field-finder.js
-
