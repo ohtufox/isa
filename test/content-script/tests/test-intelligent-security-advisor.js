3eb2ac8551bfabd65b34f14bf1b8933ed0f20479
Juho Kallio <juhopkallio@gmail.com>
2013-02-21 14:28:54 +0200

    Working tests for mutation observer.

--- test/content-script/tests/test-intelligent-security-advisor.js
+++ test/content-script/tests/test-intelligent-security-advisor.js
@@ -1,6 +1,5 @@
 intelligentSecurityAdvisor.init();
 
-
 test( 'Standard password click peek test', function() {
     let testElement = document.getElementById('password1');
     ok( testElement.type === 'password', 'Element is originally of the type of password' );
@@ -8,16 +7,14 @@ test( 'Standard password click peek test', function() {
     ok( testElement.type === 'text', 'After a click element is of the type of text' );
 });
 
-test( 'JavaScript generated password field click peek test', function() {
-intelligentSecurityAdvisor.init();
+asyncTest( 'JavaScript generated password field click peek test', function() {
     let container = document.getElementById('passwordFieldContainer');
-    container.innerHTML += 'i';
-    console.log('new password field start');
     container.innerHTML += '<input id=\"password4\" type=\"password\"/>';
-    console.log('new password field');
     let testElement = document.getElementById('password4');
     ok( testElement.type === 'password', 'Element is originally of the type of password' );
-    console.log('click');
-    $('#password4').click();
-    ok( testElement.type === 'text', 'After a click element is of the type of text' );
+    setTimeout(function() {
+        $('#password4').click();
+        ok( testElement.type === 'text', 'After a click element is of the type of text' );
+        start();
+    }, 50);
 });
