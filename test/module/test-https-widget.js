a429bb4bd5459c543f95d1df077048ed60f54d65
Carolina <carolina.lindqvist@cs.helsinki.fi>
2013-02-22 16:11:52 +0200

    test for https on current page

--- test/module/test-https-widget.js
+++ test/module/test-https-widget.js
@@ -1,14 +1,37 @@
 let https = require("https-checker.js");
 let tabs = require("sdk/tabs"); // Get tabs
+let browser = require("sdk/deprecated/tab-browser");
 
-exports["detect https from URL"] = function(assert) {
+exports.http = function(assert) {
+  assert.waitUntilDone();
+    let url = "data:text/html";
+    require("sdk/deprecated/tab-browser").addTab(
+      url,
+      {
+        onLoad: function(e) {
+          assert.assertEqual(tabs.activeTab.url, url);
+          let connectionState = https.isSecure(tabs.activeTab.url);
+          assert.assertEqual(connectionState, "Insecure connection");
+          assert.done();
+        }
+      }
+    );
+};
 
-      // Go to http website
-      let insecureUrl = 'google.com';
-      tabs.open(insecureUrl);
+exports.https = function(assert) {
+  assert.waitUntilDone();
+    let url = "https://github.com/";
+    require("sdk/deprecated/tab-browser").addTab(
+      url,
+      {
+        onLoad: function(e) {
+          assert.assertEqual(tabs.activeTab.url, url);
+          let connectionState = https.isSecure(tabs.activeTab.url);
+          assert.assertEqual(connectionState, "Secure connection");
+          assert.done();
+        }
+      }
+    );
+};
 
-      // Get site state
-      let connectionState = https.isSecure(tabs.activeTab.url);
-      assert.assertEqual(connectionState, "Insecure connection");
-      tabs.activeTab.close;
-}
+
