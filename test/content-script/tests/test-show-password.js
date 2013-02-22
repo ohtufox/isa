e6dd15162e6d7d150d301aec957d8dd539b4ab50
ohtufox <tmalmivi@cs.helsinki.fi>
2013-02-22 15:20:46 +0200

    some changes to tests

--- test/content-script/tests/test-show-password.js
+++ test/content-script/tests/test-show-password.js
@@ -1,15 +1,20 @@
-var inputFields = document.getElementsByTagName('input');
-var element = inputFields[0];
+const TEST_PEEK_TIME = 10;
+const TEST_PEEK_TIME_INCREMENT = 1;
 
-test( "show password in text", function() {
-	showPwd.peekPassword(element, 10);
-	ok( element.type === "text", "input is changed to text" );
+test( 'show password in text', function() {
+	let element = document.getElementById('pwdfield1');
+	ok( element.type === 'password', 'at first input is shown as password' );
+	showPwd.peekPassword(element, TEST_PEEK_TIME);
+	ok( element.type === 'text', 'input is changed to text' );
 });
 
-asyncTest("after peek hide password", function() {
-	showPwd.peekPassword(element, 10);
+asyncTest('after peek hide password', function() {
+	let element = document.getElementById('pwdfield2');
+	ok( element.type === 'password', 'at first input is shown as password' );
+	showPwd.peekPassword(element, TEST_PEEK_TIME);
+	ok( element.type === 'text', 'input is changed to text' );
 	setTimeout(function() {
-        ok( element.type === "password", "after given time input is password" );
+        ok( element.type === 'password', 'after given time input is changed back to password' );
         start();
-    }, 11);
+    }, TEST_PEEK_TIME + TEST_PEEK_TIME_INCREMENT);
 });
