3a8920b492bef3b07494438d8b22f94b1c7cc736
Erno Hopearuoho <erno.hopearuoho@gmail.com>
2013-03-07 19:10:51 +0200

    Show different icon based on site HTTPS status

--- data/show-password.js
+++ data/show-password.js
@@ -1,4 +1,13 @@
 (function(showPwd, finder, undefined){
+    self.port.on("ready", function() {
+        showPwd.fixPage(3000);
+    });
+
+    let sitestatus = "";
+
+    self.port.on("status", function(status) {
+        sitestatus = status;
+    });
 
     showPwd.peekPassword = function(element, time) {
 		if(element.value.length >0) {
@@ -20,7 +29,11 @@
 
     function addIcon(element, time) {
         element.style.cssText = "background-size: auto 100%, auto;";
-        element.style.backgroundImage = "url(http://i.imgur.com/VAZE0IX.png)";
+        if (sitestatus.httpstatus == "HTTPS") {
+            element.style.backgroundImage = "url(" + sitestatus.good + ")";
+        } else {
+            element.style.backgroundImage = "url(" + sitestatus.bad + ")";
+        }
         element.style.paddingLeft = "0px";
         element.style.backgroundPosition = "100% 50%, 100% 50%";
         element.style.backgroundRepeat = "no-repeat";
@@ -29,7 +42,6 @@
             showPwd.peekPassword(element, time);
             element.focus();
         });
-
     }
 
     function showPassword(element){
@@ -39,6 +51,5 @@
     function hidePassword(element){
             element.type = 'password';
     }
-
 
 }(window.showPwd = window.showPwd || {}, fieldFinder));
