cae5892e8dde6bbd7718b38da486696b80164d57
Titti M <tmalmivi@cs.helsinki.fi>
2013-03-07 18:41:32 +0200

    some formatting

--- data/show-password.js
+++ data/show-password.js
@@ -1,9 +1,16 @@
+
 (function(showPwd, finder, icon, undefined){
     showPwd.peekPassword = function(element, time) {
         if(element.value.length >0) {
+<<<<<<< HEAD
             showPassword(element);
             setTimeout(hidePassword, time, element);		
         }		
+=======
+		    showPassword(element);
+		    setTimeout(hidePassword, time, element);		
+	    }		
+>>>>>>> some formatting
     };
 	
     showPwd.fixPage = function(status, time) {
@@ -18,7 +25,7 @@
         icon.add(element, time);
         element.addEventListener('click', function(e) {
             //showPwd.peekPassword(element, time);
-			isThisDoneBefore(element, time);
+		    isThisDoneBefore(element, time);
         });
     };
 
@@ -30,11 +37,31 @@
 
     function showPassword(element){	
 	    element.type = 'text';
-    }
 	
-
     function hidePassword(element){
         element.type = 'password';
     }
+	
+    function isThisDoneBefore(element, time) {
+        let msg = "clicking this will show password in cleartext for 3 secs";
+        self.port.emit('isUsed', msg);
+        self.port.on('storage-status', function(status) {
+            console.log("viesti saatu storagelta:");
+            console.log(status);
+            self.port.emit('used', msg);
+
+            if(!status) {
+                console.log("event for the first time");
+				
+                self.port.emit('used', msg);	
+            }
+			
+            else {
+                console.log("event happened before");
+                showPwd.peekPassword(element, time);
+            }
+        });	
+    }
+		
     
 }(window.showPwd = window.showPwd || {}, fieldFinder, fieldIcon));
