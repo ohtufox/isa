6c1f82ae5c74c04fc6851f46cd45a96aa09fa29e
Erno Hopearuoho <erno.hopearuoho@gmail.com>
2013-03-07 17:15:17 +0200

    Easy way out, nonclickable icon

--- data/show-password.js
+++ data/show-password.js
@@ -1,6 +1,6 @@
 (function(showPwd, finder, undefined){
 
-	showPwd.peekPassword = function(element, time) {
+    showPwd.peekPassword = function(element, time) {
 		if(element.value.length >0) {
 			showPassword(element);
 			setTimeout(hidePassword, time, element);		
@@ -14,43 +14,22 @@
         }
     };
 
-    function fixElement(element, time) {
+    showPwd.fixElement = function(element, time) {
         addIcon(element, time);
     }
 
-    function insertAfter(referenceNode, newNode) {
-            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    }
-
     function addIcon(element, time) {
-        let icon = document.createElement("input");
-        icon.type = "button";
-        icon.class = "isa-pwicon";
+        element.style.cssText = "background-size: auto 100%, auto;";
+        element.style.backgroundImage = "url(http://i.imgur.com/VAZE0IX.png)";
+        element.style.paddingLeft = "0px";
+        element.style.backgroundPosition = "100% 50%, 100% 50%";
+        element.style.backgroundRepeat = "no-repeat";
 
-        icon.style.cssText = "background-size: auto 100%, auto;";
-        icon.style.border = "none";
-        icon.style.backgroundImage = "url(http://i.imgur.com/VAZE0IX.png)";
-        icon.style.backgroundPosition = "center center";
-        icon.style.backgroundRepeat = "no-repeat";
-        icon.style.backgroundColor = "transparent";
-        icon.style.cursor = "pointer";
-        icon.style.position = "absolute";
-
-        let height = element.offsetHeight;
-        icon.style.width = height + "px";
-        icon.style.height = height + "px";
-
-        let right = element.offsetLeft + element.offsetWidth;
-        let top = element.offsetTop;
-        icon.style.top = top + "px";
-        icon.style.left = right-height +"px";
-
-        icon.addEventListener('click', function(e) {
+        element.addEventListener('click', function(e) {
             showPwd.peekPassword(element, time);
             element.focus();
         });
 
-        insertAfter(element, icon);
     }
 
     function showPassword(element){
