e6f0626426634b234e6aa0f54125ae007f1d92e8
Erno Hopearuoho <erno.hopearuoho@gmail.com>
2013-03-13 04:13:25 +0200

    Updated selenium

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -O selenium.jar https://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar
+wget -O selenium.jar https://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
 go get bitbucket.org/tebeka/selenium
 go run tests.go
