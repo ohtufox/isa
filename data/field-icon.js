104ac399e1826ccdb6f8d6faea27803f9c14b1bf
Erno Hopearuoho <erno.hopearuoho@gmail.com>
2013-04-04 17:50:58 +0300

    Removed useless time variable, fixed anchoring

--- data/field-icon.js
+++ data/field-icon.js
@@ -9,65 +9,38 @@
         preferences = data.preferences;
     }
 
-    fieldIcon.displayOnFocus = function(element, target) {
-        element.addEventListener('focus', function(e) {
-            target.style.visibility = "visible";
-        });
-
-        element.addEventListener('blur', function(e) {
-            target.style.visibility = "hidden";
-        });
-    }
-
-    function insertAfter(referenceNode, newNode) {
-            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    }
-
     fieldIcon.closePanel = function() {
         setTimeout(function() {
             self.port.emit('close-panel');
         }, 3000);        
     }
 
-    fieldIcon.add = function(element, time) {
+    fieldIcon.add = function(element) {
         let imgi=document.createElement("img");
+
+        addPanelListener(element, imgi);
+        if (httpStatus == "HTTPS") {
+            // Secure site, display clickable eye icon.
+            if(preferences.passwordPeek) {
+                addPeekingEye(element, imgi);
+            }
+        } else {
+            addWarningIcon(element, imgi);
+        }
+
         imgi.style.position = 'absolute';
 
         let _body = document.getElementsByTagName('body') [0];
         _body.appendChild(imgi);
 
-        if (httpStatus == "HTTPS") {
-            // Secure site, display clickable eye icon.
-            if(preferences.passwordPeek) {
-                displayPeekingEye(element, imgi);
-            }
-        } else {
-            // Unsecure site, display warning icon.
-            imgi.setAttribute('src', icons.bad);
-            // ADD INFOPANEL LISTENER HERE
-            imgi.className += " anchorclass";
-            imgi.addEventListener('click', function(e) { 
-                if(httpStatus == "HTTPS" && element.value.length >0) {            
-                    settingsChecker.isPasswordPeekedBefore(element, time);
-                    //closePanel();
-                } 
-                if(httpStatus !== "HTTPS") {
-                    self.port.emit('info', 'This is insecure form.');
-                    fieldIcon.closePanel();                
-                }
-                element.focus();
-            });
-        }
-
-        // Position the icon
         let rect = element.getBoundingClientRect();
         let height = rect.bottom - rect.top;
+        // If the page is scrolled when its loaded we need to take into account the offset
         imgi.style.left = window.pageXOffset + rect.right-height + 'px';
         imgi.style.top = window.pageYOffset + rect.top + 'px';
         
         let size = height;
         imgi.style.width = size + 'px';
-
 
         window.addEventListener('resize', function(e) {
             let rect = element.getBoundingClientRect();
@@ -77,10 +50,35 @@
         });
     }
 
-    function displayPeekingEye(element, imgi) {
+    function addPanelListener(element, imgi) {
+            imgi.addEventListener('click', function(e) { 
+                if(httpStatus !== "HTTPS") {
+                    imgi.className = "anchorclass";
+                    e.preventDefault();
+                    self.port.emit('info', 'This is insecure form.');
+                    fieldIcon.closePanel();                
+                }
+            });
+    }
+
+   function addWarningIcon(element, imgi) {
+            imgi.setAttribute('src', icons.bad);
+   }
+
+   function  displayOnFocus(element, target) {
+        element.addEventListener('focus', function(e) {
+            target.style.visibility = "visible";
+        });
+
+        element.addEventListener('blur', function(e) {
+            target.style.visibility = "hidden";
+        });
+    }
+
+    function addPeekingEye(element, imgi) {
         imgi.setAttribute('src', icons.eyeclosed);
         imgi.style.visibility = 'hidden';
-        fieldIcon.displayOnFocus(element, imgi);
+        displayOnFocus(element, imgi);
         addEyeAnimation(imgi);
         showPwd.addMouseDownPeekListener(imgi, element);
     }
