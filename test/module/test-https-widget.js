6f2e6189dbe54b5e110f8823b2da67b558fdc74a
Carolina <carolina.lindqvist@cs.helsinki.fi>
2013-04-11 18:07:09 +0300

    test update

--- test/module/test-https-widget.js
+++ test/module/test-https-widget.js
@@ -1,38 +1,43 @@
 let https = require('https-checker.js');
-let tabs = require('sdk/tabs'); // Get tabs
-let browser = require('sdk/deprecated/tab-browser');
+let tabs = require('sdk/tabs');
+let browser = require('sdk/tabs/utils');
 let testHelper = require('test-helper');
+let utils = require("window/utils");
 
-
+let HTTP_URL = testHelper.getHtmlFolder(module.uri) + '/test.html';
+let HTTPS_URL = 'https://www.eff.org/';
+/*
 exports.http = function(assert) {
-  assert.waitUntilDone();
-    let url = testHelper.getHtmlFolder(module.uri) + '/test.html';;
-    require('sdk/deprecated/tab-browser').addTab(
-      url,
-      {
-        onLoad: function(e) {
-          assert.assertEqual(tabs.activeTab.url, url);
-          let connectionState = https.isSecure(tabs.activeTab.url);
-          assert.assertEqual(connectionState, 'HTTP');
-          assert.done();
-        }
-      }
-    );
-};
+       assert.waitUntilDone();
+       let url = HTTP_URL;
 
-// exports.https = function(assert) {
-  // assert.waitUntilDone();
-    // let url = 'https://github.com/';
-    // require('sdk/deprecated/tab-browser').addTab(
-      // url,
-      // {
-        // onLoad: function(e) {
-          // assert.assertEqual(tabs.activeTab.url, url);
-          // let connectionState = https.isSecure(tabs.activeTab.url);
-          // assert.assertEqual(connectionState, 'HTTPS');
-          // assert.done();
-        // }
-      // }
-    // );
-// };
+       let tab = tabs.open(url);
+       tabs.on('ready', function(tab) {
+       assert.assertEqual(tabs.activeTab.url, url);
+
+       let window = utils.getMostRecentBrowserWindow();       
+       let connectionState = https.isSecure(window);
+       assert.assertEqual(connectionState, 'HTTP');
+       assert.done();
+    });
+};
+*/
+exports.https = function(assert) {
+       assert.waitUntilDone();
+       let url2 = HTTPS_URL;
+
+       console.log("testing https");
+       let tab = tabs.open(url2);
+       
+       tabs.on('ready', function(tab) {
+       console.log("site ready, asserting");
+       assert.assertEqual(tab.url, url2);
+       console.log("assertion complete " +tab.url);
+
+       let window = utils.getMostRecentBrowserWindow();       
+       let connectionState = https.isSecure(window);
+       assert.assertEqual(connectionState, 'HTTPS');
+       assert.done();
+    });
+};
 
